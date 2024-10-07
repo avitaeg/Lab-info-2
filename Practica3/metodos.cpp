c5f6cc9a44f2fec7b72a58c45ac90fe3dd78135f
avitaeg <elvia.gomez1@udea.edu.co>
2024-10-07 01:28:12 -0500

    Add files via upload

---
+++ Practica3/metodos.cpp
@@ -0,0 +1,12 @@
+#include "funciones.h"
+
+
+int main(){
+
+    menu();
+
+    //cout << endl << cadenaCodifMet1("1354");      //Comprobar la codificación con la cadena que se ingrese como parámetro
+    //modificar(cadenaCodifMet1("1100101101101111011011110110111101101111"));    //Se va a solicitar el nombre del archivo donde se quiere ver reflejada la codificación pruebaCodificado
+
+    return 0;
+}
